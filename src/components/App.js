5bbc7230d8900c74afdbd492adacbf214044dec2
Elijah Pamintuan <es.pamintuan@jumpdigital.asia>
2019-04-08 16:14:49 +0800

    Init push

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PostList from './PostList';
+
+
+const App = () => {
+	return (
+		<div className="ui containeer">
+			<PostList/>
+		</div>
+	);
+}
+
+export default App;
